f054ef6f4cc02582ac6e077a4dec96ec7cfb25ca
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-03-25 21:19:32 -0500

    More hackerrank exercises

---
+++ Java/StdinAndStdOut2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            String extra=sc.nextLine();
+            double y=sc.nextDouble();
+            String extra2=sc.nextLine();
+            String s=sc.nextLine();
+
+            System.out.println("String: "+s);
+            System.out.println("Double: "+y);
+            System.out.println("Int: "+x);
+    }
+}
