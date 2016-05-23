4e0c00dc8580598e6af9081c5c86bf354ca92cfb
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-05-22 20:12:46 -0500

    Day 16 of the 30 day challenge

---
+++ 30_Days_Of_Code/Day16_Exceptions_StringToInt.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+	try
+	{
+		int no;
+		no = stoi(S);
+		cout<<no<<endl;
+	}
+	catch(exception a)
+	{
+		cout << "Bad String";
+	}
+	return 0;
+}
